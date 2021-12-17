b01cad4f2a9c51c855d557a0ed1734ed98f34a65
kiritotsunemoto <95726553+kiritotsunemoto@users.noreply.github.com>
2021-12-17 10:39:57 +0900

    Add files via upload

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -18,3 +18,14 @@ $(function () {
   });
 
 });
+
+const CLASSNAME = "-visible";
+const TIMEOUT = 1500;
+const $target = $(".title");
+
+setInterval(() => {
+  $target.addClass(CLASSNAME);
+  setTimeout(() => {
+    $target.removeClass(CLASSNAME);
+  }, TIMEOUT);
+}, TIMEOUT * 2);
